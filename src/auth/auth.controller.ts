3eb13a73b20fdf48428e0f9d825ec2afa8d61dd5
Zotoff <paulzotoff@gmail.com>
2024-03-02 18:55:08 +0300

    JWT logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,10 +12,14 @@ import { RegistrationGuard } from './guards/registration.guard';
 import { Response } from 'express';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoginGuard } from './guards/login.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(RegistrationGuard)
   @Post('registration')
@@ -35,8 +39,16 @@ export class AuthController {
   async loginUser(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
     const user = await this.usersService.login(loginUserDto);
 
+    // generate access token for user
+    const access = await this.authService.generateAccessToken(user);
+
+    // generate refresh token
+    const refresh = await this.authService.generateRefreshToken(user._id as string);
+
     res.statusCode = HttpStatus.OK;
     return res.send({
+      ...access,
+      ...refresh,
       username: user.username,
     });
   }
